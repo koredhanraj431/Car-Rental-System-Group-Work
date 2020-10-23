bae9bc5516aec24a8ea961b42e2d2956431b4d59
vinayakkokane <51049381+vinayakkokane@users.noreply.github.com>
2020-10-23 17:12:19 +0530

    Create register.php

---
+++ Car-Rental-System(EDI)/User/register.php
@@ -0,0 +1,47 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Signup - Drive Your Dreams</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <!-- FontAwesome -->
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+  <link rel="icon" href="images/favicon-16x16.png">
+  <link rel="stylesheet" type="text/css" href="css/regStyles.css">
+</head>
+<body>
+  <div class="header">
+  	<h2><b>Sign Up here!</b></h2>
+    <p>Get started with India's leading and one stop Car rental platform!</p>
+  </div>
+
+  <form method="post" action="register.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  	  <label>Username</label>
+  	  <input type="text" name="username" value="<?php echo $username; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Email</label>
+  	  <input type="email" name="email" value="<?php echo $email; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Password</label>
+  	  <input type="password" name="password_1">
+  	</div>
+  	<div class="input-group">
+  	  <label>Confirm password</label>
+  	  <input type="password" name="password_2">
+  	</div>
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Register</button>
+  	</div>
+  	<p>
+  		Already our customer? <a href="login.php">Sign in</a>
+  	</p>
+  </form>
+</body>
+</html>
