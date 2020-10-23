76c073298aa2d8f9abe98f991ec359a2102849a0
vinayakkokane <51049381+vinayakkokane@users.noreply.github.com>
2020-10-23 17:11:23 +0530

    Update server.php

--- Car-Rental-System(EDI)/User/server.php
+++ Car-Rental-System(EDI)/User/server.php
@@ -7,10 +7,10 @@ $email    = "";
 $errors = array();
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'cars');
+$db = mysqli_connect('localhost', 'root', '', 'cr');
 
 // REGISTER USER
-if (isset($_POST['submit'])) {
+if (isset($_POST['reg_user'])) {
   // receive all input values from the form
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $email = mysqli_real_escape_string($db, $_POST['email']);
@@ -23,7 +23,7 @@ if (isset($_POST['submit'])) {
   if (empty($email)) { array_push($errors, "Email is required"); }
   if (empty($password_1)) { array_push($errors, "Password is required"); }
   if ($password_1 != $password_2) {
-	array_push($errors, "The two passwords do not match");
+	array_push($errors, "Passwords aren't matching!");
   }
 
   // first check the database to make sure
@@ -54,3 +54,32 @@ if (isset($_POST['submit'])) {
   	header('location: index.php');
   }
 }
+
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['username'] = $username;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: index.php');
+  	}else {
+  		array_push($errors, "Oops! username/password combination isn't correct");
+  	}
+  }
+}
+
+?>
