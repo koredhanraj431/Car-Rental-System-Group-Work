cc788747e3340586c24a26efa810d4f85ad531ef
vinayakkokane <51049381+vinayakkokane@users.noreply.github.com>
2020-10-25 09:45:24 +0530

    Update carvarieties.php

--- Car-Rental-System(EDI)/User/carvarieties.php
+++ Car-Rental-System(EDI)/User/carvarieties.php
@@ -10,6 +10,107 @@
     <title>Explore cars</title>
   </head>
   <body>
-    <h1>All varieties of cars that you want for today's drive.</h1>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">Drive Your Dream</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="aboutus.php">About us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contact us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="carvarieties.php">Explore cars</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="register.php"><i class="fas fa-user"></i>  Register<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt"></i>  Login <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- heading -->
+    <div class="container-fluid">
+      <br>
+      <br> <br>
+      <div class="header">
+        <h1 style="text-align: center; "> Pick your cars from here , Drive your dreams!</h1>
+      </div>
+      <hr>  <br>
+      <div class="container-explore" style="text-align: center;">
+        <div class="row">
+          <div class="col-lg-3">
+            <img style="border-radius: 6px; height: 140px; width: 250px;" src="images/car3.jpg" alt="" >
+            <h2 style="">Economy</h2>
+            <p>
+              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+            </p>
+          </div>
+          <div class="col-lg-3">
+            <img style="border-radius: 6px; height: 140px; width: 250px;" src="images/car7.jpg" alt="" >
+            <h2 style="">Standard</h2>
+            <p>
+              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+            </p>
+          </div>
+          <div class="col-lg-3">
+            <img style="border-radius: 6px; height: 140px; width: 250px;" src="images/car6.jpg" alt="" >
+            <h2 style="">Standard SUV</h2>
+            <p>
+              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+            </p>
+          </div>
+          <div class="col-lg-3">
+            <img style="border-radius: 6px; height: 140px; width: 250px;" src="images/car11.jpg" alt="" >
+            <h2 style="">Premium</h2>
+            <p>
+              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+            </p>
+          </div>
+          <div class="col-lg-3">
+            <img style="border-radius: 6px; height: 140px; width: 200px;" src="images/car2.png" alt="" >
+            <h2 style="">PickUp</h2>
+            <p>
+              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+            </p>
+          </div>
+          <div class="col-lg-3">
+            <img style="border-radius: 6px; height: 140px; width: 200px;" src="images/car4.png" alt="" >
+            <h2 style="">Minivan</h2>
+            <p>
+              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+            </p>
+          </div>
+          <div class="col-lg-3">
+            <img style="border-radius: 6px; height: 140px; width: 200px;" src="images/car5.png" alt="" >
+            <h2 style="">Traveller</h2>
+            <p>
+              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+            </p>
+          </div>
+          <div class="col-lg-3">
+            <img style="border-radius: 6px; height: 140px; width: 180px;" src="images/car9.jpg" alt="" >
+            <h2 style="">Stylish</h2>
+            <p>
+              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
