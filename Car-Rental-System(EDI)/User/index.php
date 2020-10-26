b6290a474532191c5716ffee954a1065e50320d4
koredhanraj431 <54303198+koredhanraj431@users.noreply.github.com>
2020-10-26 14:51:15 +0530

    Update index.php

--- Car-Rental-System(EDI)/User/index.php
+++ Car-Rental-System(EDI)/User/index.php
@@ -67,44 +67,53 @@
       </div>
     </nav>
     <!-- Sliding images -->
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="images/mainImg.jpg" class="d-block w-100" alt="first slide">
-          <div class="carousel-caption">
-            <h1><b>Wide variety</b></h1>
-            <h3>Any car you want, we have it!</h3>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="images/img2.jpg" class="d-block w-100" alt="second slide">
-          <div class="carousel-caption">
-            <h1><b>Plan for your Trips!</b></h1>
-            <h3 >Take only memories, leave only footprints</h3>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="images/img3.jpg" class="d-block w-100" alt="Third slide">
-          <div class="carousel-caption">
-            <h1><b>Low cost car rental</b></h1>
-            <h3>Pay less and Explore more!</h3>
-          </div>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="images/mainImg.jpg" class="d-block w-100" alt="First slide">
+        <div class="carousel-caption">
+          <h1><b>Wide variety</b></h1>
+          <h3>Any car you want, we have it!</h3>
         </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+      <div class="carousel-item">
+        <img src="images/img2.jpg" class="d-block w-100" alt="second slide">
+        <div class="carousel-caption">
+          <h1><b>Plan for your Trips!</b></h1>
+          <h3>Take only memories, leave only footprints</h3>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="images/img3.jpg" class="d-block w-100" alt="Third slide">
+        <div class="carousel-caption">
+          <h1><b>Low cost car rental</b></h1>
+          <h3>Pay less and Explore more!</h3>
+        </div>
+      </div>
+      <div class="carousel-item">
+            <img src="images/img4.jpg" class="d-block w-100" alt="Fourth slide">
+            <div class="carousel-caption">
+                <h1>Start your wonderful journey</h1><br>
+                <h3> We simplified car rentals, so you can focus on what's important to you.
+                </h3>
+            </div>
+        </div>
     </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
     <!-- container marketing -->
     <div class="container marketing d-flex justify-content-center">
       <div class="row py-4">
