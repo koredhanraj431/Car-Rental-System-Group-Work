4c5a3c64270fa396489ea948b1b691a1f32d1697
ved1806 <50493447+ved1806@users.noreply.github.com>
2020-10-26 18:38:15 +0530

    Update carvarieties.php

--- Car-Rental-System(EDI)/User/carvarieties.php
+++ Car-Rental-System(EDI)/User/carvarieties.php
@@ -57,56 +57,56 @@
             <img style="border-radius: 6px; height: 140px; width: 250px;" src="images/car3.jpg" alt="" >
             <h2 style="">Economy</h2>
             <p>
-              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+              <a class="btn btn-default" href="Allcars.php/#Economy" role="button" style="border-color: #000;">View availability>></a>
             </p>
           </div>
           <div class="col-lg-3">
             <img style="border-radius: 6px; height: 140px; width: 250px;" src="images/car7.jpg" alt="" >
             <h2 style="">Standard</h2>
             <p>
-              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+              <a class="btn btn-default" href="Allcars.php/#Standard" role="button" style="border-color: #000;">View availability>></a>
             </p>
           </div>
           <div class="col-lg-3">
             <img style="border-radius: 6px; height: 140px; width: 250px;" src="images/car6.jpg" alt="" >
             <h2 style="">Standard SUV</h2>
             <p>
-              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+              <a class="btn btn-default" href="Allcars.php/#Standard SUV" role="button" style="border-color: #000;">View availability>></a>
             </p>
           </div>
           <div class="col-lg-3">
             <img style="border-radius: 6px; height: 140px; width: 250px;" src="images/car11.jpg" alt="" >
             <h2 style="">Premium</h2>
             <p>
-              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+              <a class="btn btn-default" href="Allcars.php/#Premium" role="button" style="border-color: #000;">View availability>></a>
             </p>
           </div>
           <div class="col-lg-3">
             <img style="border-radius: 6px; height: 140px; width: 200px;" src="images/car2.png" alt="" >
             <h2 style="">PickUp</h2>
             <p>
-              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+              <a class="btn btn-default" href="Allcars.php/#Pickup" role="button" style="border-color: #000;">View availability>></a>
             </p>
           </div>
           <div class="col-lg-3">
             <img style="border-radius: 6px; height: 140px; width: 200px;" src="images/car4.png" alt="" >
             <h2 style="">Minivan</h2>
             <p>
-              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+              <a class="btn btn-default" href="Allcars.php/#Minivan" role="button" style="border-color: #000;">View availability>></a>
             </p>
           </div>
           <div class="col-lg-3">
             <img style="border-radius: 6px; height: 140px; width: 200px;" src="images/car5.png" alt="" >
             <h2 style="">Traveller</h2>
             <p>
-              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+              <a class="btn btn-default" href="Allcars.php/#Traveller" role="button" style="border-color: #000;">View availability>></a>
             </p>
           </div>
           <div class="col-lg-3">
             <img style="border-radius: 6px; height: 140px; width: 180px;" src="images/car9.jpg" alt="" >
             <h2 style="">Stylish</h2>
             <p>
-              <a class="btn btn-default" href="#" role="button" style="border-color: #000;">View availability>></a>
+              <a class="btn btn-default" href="Allcars.php/#Stylish" role="button" style="border-color: #000;">View availability>></a>
             </p>
           </div>
         </div>
