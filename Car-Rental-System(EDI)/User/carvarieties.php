e8da85edbb5f099ee1b19fbeb0bc96b28112cddf
koredhanraj431 <54303198+koredhanraj431@users.noreply.github.com>
2020-10-27 12:57:51 +0530

    Update carvarieties.php

--- Car-Rental-System(EDI)/User/carvarieties.php
+++ Car-Rental-System(EDI)/User/carvarieties.php
@@ -16,38 +16,14 @@
   </head>
   <body>
     <!-- navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="index.php">Drive Your Dream</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="aboutus.php">About us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact.php">Contact us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="carvarieties.php">Explore cars</a>
-            </li>
-          </ul>
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="register.php"><i class="fas fa-user"></i>  Register<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt"></i>  Login <span class="sr-only">(current)</span></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <?php 
+  if (strlen($_SESSION['username'])==0){
+    include('Includes/navbar.php');
+  }
+  else{
+    include('Includes/header.php');
+  }
+  ?>
     <!-- heading -->
     <div class="container-fluid">
       <br>
