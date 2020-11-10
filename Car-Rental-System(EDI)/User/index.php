77f4c26675df48299daadcf49001989128fdae45
vinayakkokane <51049381+vinayakkokane@users.noreply.github.com>
2020-11-10 17:07:04 +0530

    Update index.php

--- Car-Rental-System(EDI)/User/index.php
+++ Car-Rental-System(EDI)/User/index.php
@@ -70,9 +70,9 @@ if (isset($_SESSION['username'])) {
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-  <footer class="p-2">
-    <p class="p-3 bg-dark text-white text-center">© Copyright 2020 Drive your Dreams. All rights reserved.</p>
-  </footer>
+  <?php
+    include('includes/footer.php');
+  ?>
 </body>
 
 </html>
