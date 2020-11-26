5238a18ca5da3402a004c10c537c3ad8be480f4f
vinayakkokane <51049381+vinayakkokane@users.noreply.github.com>
2020-11-26 23:06:28 +0530

    Update header.php

--- Car-Rental-System(EDI)/User/Includes/header.php
+++ Car-Rental-System(EDI)/User/Includes/header.php
@@ -1,13 +1,12 @@
-
-
+<?php
+session_start();
+?>
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
-    
-
 <div class="container">
-        
+
 <a class="navbar-brand" href="index.php">
 <img style="height: 50px; width: 70px; " src="images/logo1.png" alt=""> Drive Your Dreams</a>
-        
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -41,7 +40,5 @@
                     </div>
                 </li>
             </ul>
-
-
 
 </nav>
