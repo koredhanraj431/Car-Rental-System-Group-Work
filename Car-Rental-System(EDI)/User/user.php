c0d236123e2be0c584fa21647a7f968006e63540
koredhanraj431 <54303198+koredhanraj431@users.noreply.github.com>
2020-11-26 17:55:29 +0530

    Update user.php

--- Car-Rental-System(EDI)/User/user.php
+++ Car-Rental-System(EDI)/User/user.php
@@ -21,11 +21,8 @@ if (!isset($_SESSION['username'])) {
   <script src="https://kit.fontawesome.com/3695790714.js" crossorigin="anonymous"></script>
   <link rel="icon" href="images/favicon-16x16.png">
   <link rel="stylesheet" href="CSS/styles.css">
-  <title>Welcome to Drive your Dreams</title>
+  <title>Welcome, <?php  echo $_SESSION["username"]; ?></title>
 
-  <style>
-
-  </style>
 </head>
 
 <body>
@@ -49,7 +46,7 @@ if (!isset($_SESSION['username'])) {
         <h2>Explore</h2>
         <p></p>
         <p>
-          <a class="btn btn-default" href="carvarieties.php" role="button">View details>></a>
+          <a class="btn btn-default btn-outline-success" href="carvarieties.php" role="button">View details>></a>
         </p>
       </div>
       <div class="col-lg-4">
@@ -57,7 +54,7 @@ if (!isset($_SESSION['username'])) {
         <h2>Contact</h2>
         <p></p>
         <p>
-          <a class="btn btn-default" href="contact.php" role="button">View details>></a>
+          <a class="btn btn-default btn-outline-success" href="contact.php" role="button">View details>></a>
         </p>
       </div>
       <div class="col-lg-4">
@@ -65,19 +62,22 @@ if (!isset($_SESSION['username'])) {
         <h2>AboutUs</h2>
         <p></p>
         <p>
-          <a class="btn btn-default " href="aboutus.php" role="button">View details>></a>
+          <a class="btn btn-default btn-outline-success" href="aboutus.php" role="button">View details>></a>
         </p>
       </div>
     </div>
   </div>
+  <footer>
+    <?php
+      include('includes/footer.php');
+    ?>
+  </footer>
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-  <footer class="p-2">
-    <p class="p-3 bg-dark text-white text-center"> Copyright © 2020 Drive your Dreams. All rights reserved.</p>
-  </footer>
+  
 </body>
 
 </html>
