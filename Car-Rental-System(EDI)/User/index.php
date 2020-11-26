4e80855c63291431003b424640d2203e9c991dfe
koredhanraj431 <54303198+koredhanraj431@users.noreply.github.com>
2020-11-26 17:56:03 +0530

    Update index.php

--- Car-Rental-System(EDI)/User/index.php
+++ Car-Rental-System(EDI)/User/index.php
@@ -3,6 +3,7 @@ session_start();
 
 if (isset($_SESSION['username'])) {
   header('location: user.php');
+  exit();
 }
 ?>
 
@@ -17,6 +18,8 @@ if (isset($_SESSION['username'])) {
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <!-- FontAwesome -->
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+
 
   <script src="https://kit.fontawesome.com/3695790714.js" crossorigin="anonymous"></script>
   <link rel="icon" href="images/favicon-16x16.png">
@@ -34,45 +37,41 @@ if (isset($_SESSION['username'])) {
     include('Includes/carousel.php');
     ?>
   <!-- container marketing -->
-  <div class="container marketing d-flex justify-content-center py-5 mt-5 mb-3">
+  <div class="container marketing d-flex justify-content-center">
     <div class="row py-4">
-      <div class="col-lg-4 ">
+      <div class="col-lg-4">
         <img class="img-circle" src="images/explore.png" alt="Explore">
-        <h2 class="mt-2">Explore</h2>
+        <h2>Explore</h2>
         <p></p>
         <p>
-          <a type="button" class="btn btn-outline-success" href="carvarieties.php" role="button">View details >></a>
+          <a class="btn btn-default btn-outline-success" href="carvarieties.php" role="button">View details>></a>
         </p>
       </div>
-      <div class="col-lg-4 ">
+      <div class="col-lg-4">
         <img class="img-circle" src="images/contact.png" alt="Contact">
-        <h2 class="mt-2">Contact</h2>
+        <h2>Contact</h2>
         <p></p>
         <p>
-          <a type="button" class="btn btn-outline-success" href="contact.php" role="button">View details >></a>
+          <a class="btn btn-default btn-outline-success" href="contact.php" role="button">View details>></a>
         </p>
       </div>
       <div class="col-lg-4">
         <img class="img-circle" src="images/about.jpg" alt="about us">
-        <h2 class="mt-2">AboutUs</h2>
+        <h2>AboutUs</h2>
         <p></p>
         <p>
-          <a type="button" class="btn btn-outline-success"href="aboutus.php" role="button">View details >></a>
+          <a class="btn btn-default btn-outline-success" href="aboutus.php" role="button">View details>></a>
         </p>
       </div>
     </div>
   </div>
-
+ 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-  <footer>
-    <?php
-      include('includes/footer.php');
-    ?>
-  </footer>
+  
 </body>
 
 </html>
