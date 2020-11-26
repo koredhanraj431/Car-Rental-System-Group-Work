ab3d371e704113f1de0377345cf62ead75c3c2d8
koredhanraj431 <54303198+koredhanraj431@users.noreply.github.com>
2020-11-26 18:01:50 +0530

    Update index.php

--- Car-Rental-System(EDI)/User/index.php
+++ Car-Rental-System(EDI)/User/index.php
@@ -65,7 +65,11 @@ if (isset($_SESSION['username'])) {
       </div>
     </div>
   </div>
- 
+ <footer>
+    <?php
+      include('includes/footer.php');
+    ?>
+  </footer>
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
