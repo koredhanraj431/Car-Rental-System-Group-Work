dd80434f66e2704d0453f6042742a56dcb8bde68
koredhanraj431 <54303198+koredhanraj431@users.noreply.github.com>
2020-11-26 17:56:47 +0530

    Update footer.php

--- Car-Rental-System(EDI)/User/Includes/footer.php
+++ Car-Rental-System(EDI)/User/Includes/footer.php
@@ -1,180 +1,214 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <!-- FontAwesome -->
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <script src="https://kit.fontawesome.com/3695790714.js" crossorigin="anonymous"></script>
-  </head>
-  <style media="screen">
-      ul {
+
+<head>
+    <style>
+    ul {
         margin: 0px;
         padding: 0px;
-      }
-      .footer-section {
-      background: #151414;
-      position: relative;
-      }
-      .footer-cta {
-      border-bottom: 1px solid #373636;
-      }
-      .single-cta i {
-      color: #ff5e14;
-      font-size: 30px;
-      float: left;
-      margin-top: 8px;
-      }
-      .cta-text {
-      padding-left: 15px;
-      display: inline-block;
-      }
-      .cta-text h4 {
-      color: #fff;
-      font-size: 20px;
-      font-weight: 600;
-      margin-bottom: 2px;
-      }
-      .cta-text span {
-      color: #757575;
-      font-size: 15px;
-      }
-      .footer-content {
-      position: relative;
-      z-index: 2;
-      }
-      .footer-pattern img {
-      position: absolute;
-      top: 0;
-      left: 0;
-      height: 200px;
-      background-size: cover;
-      background-position: 100% 100%;
-      }
-      .footer-logo {
-      margin-bottom: 30px;
-      }
-      .footer-logo img {
+    }
+
+    .footer-section {
+        background: #151414;
+        position: relative;
+    }
+
+    .footer-cta {
+        border-bottom: 1px solid #373636;
+    }
+
+    .single-cta i {
+        color: #ff5e14;
+        font-size: 30px;
+        float: left;
+        margin-top: 8px;
+    }
+
+    .cta-text {
+        padding-left: 15px;
+        display: inline-block;
+    }
+
+    .cta-text h4 {
+        color: #fff;
+        font-size: 20px;
+        font-weight: 600;
+        margin-bottom: 2px;
+    }
+
+    .cta-text span {
+        color: #757575;
+        font-size: 15px;
+    }
+
+    .footer-content {
+        position: relative;
+        z-index: 2;
+    }
+
+    .footer-pattern img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 200px;
+        background-size: cover;
+        background-position: 100% 100%;
+    }
+
+    .footer-logo {
+        margin-bottom: 30px;
+    }
+
+    .footer-logo img {
         max-width: 200px;
-      }
-      .footer-text p {
-      margin-bottom: 14px;
-      font-size: 14px;
-          color: #7e7e7e;
-      line-height: 28px;
-      }
-      .footer-social-icon span {
-      color: #fff;
-      display: block;
-      font-size: 20px;
-      font-weight: 700;
-      font-family: 'Poppins', sans-serif;
-      margin-bottom: 20px;
-      }
-      .footer-social-icon a {
-      color: #fff;
-      font-size: 16px;
-      margin-right: 15px;
-      }
-      .footer-social-icon i {
-      height: 40px;
-      width: 40px;
-      text-align: center;
-      line-height: 38px;
-      border-radius: 50%;
-      }
-      .facebook-bg{
-      background: #3B5998;
-      }
-      .twitter-bg{
-      background: #55ACEE;
-      }
-      .google-bg{
-      background: #DD4B39;
-      }
-      .footer-widget-heading h3 {
-      color: #fff;
-      font-size: 20px;
-      font-weight: 600;
-      margin-bottom: 40px;
-      position: relative;
-      }
-      .footer-widget-heading h3::before {
-      content: "";
-      position: absolute;
-      left: 0;
-      bottom: -15px;
-      height: 2px;
-      width: 50px;
-      background: #ff5e14;
-      }
-      .footer-widget ul li {
-      display: inline-block;
-      float: left;
-      width: 50%;
-      margin-bottom: 12px;
-      }
-      .footer-widget ul li a:hover{
-      color: #ff5e14;
-      }
-      .footer-widget ul li a {
-      color: #878787;
-      text-transform: capitalize;
-      }
-      .subscribe-form {
-      position: relative;
-      overflow: hidden;
-      }
-      .subscribe-form input {
-      width: 100%;
-      padding: 14px 28px;
-      background: #2E2E2E;
-      border: 1px solid #2E2E2E;
-      color: #fff;
-      }
-      .subscribe-form button {
+    }
+
+    .footer-text p {
+        margin-bottom: 14px;
+        font-size: 14px;
+        color: #7e7e7e;
+        line-height: 28px;
+    }
+
+    .footer-social-icon span {
+        color: #fff;
+        display: block;
+        font-size: 20px;
+        font-weight: 700;
+        font-family: 'Poppins', sans-serif;
+        margin-bottom: 20px;
+    }
+
+    .footer-social-icon a {
+        color: #fff;
+        font-size: 16px;
+        margin-right: 15px;
+    }
+
+    .footer-social-icon i {
+        height: 40px;
+        width: 40px;
+        text-align: center;
+        line-height: 38px;
+        border-radius: 50%;
+        border: 2px solid white;
+    }
+
+    .facebook-bg {
+        background: #3B5998;
+    }
+
+    .twitter-bg {
+        background: #55ACEE;
+    }
+
+    .google-bg {
+        background: #DD4B39;
+    }
+
+    .instagram-bg {
+    background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%); 
+    background: -webkit-radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);    
+}
+    .footer-widget-heading h3 {
+        color: #fff;
+        font-size: 20px;
+        font-weight: 600;
+        margin-bottom: 40px;
+        position: relative;
+    }
+
+    .footer-widget-heading h3::before {
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: -15px;
+        height: 2px;
+        width: 50px;
+        background: #ff5e14;
+    }
+
+    .footer-widget ul li {
+        display: inline-block;
+        width: 50%;
+        margin-bottom: 12px;
+    }
+
+    .footer-widget ul li a:hover {
+        color: #ff5e14;
+    }
+
+    .footer-widget ul li a {
+        color: #878787;
+        text-transform: capitalize;
+    }
+
+    .subscribe-form {
+        position: relative;
+        overflow: hidden;
+    }
+
+    .subscribe-form input {
+        width: 100%;
+        padding: 14px 28px;
+        background: #2E2E2E;
+        border: 1px solid #2E2E2E;
+        color: #fff;
+    }
+
+    .subscribe-form button {
         position: absolute;
         right: 0;
         background: #ff5e14;
         padding: 13px 20px;
         border: 1px solid #ff5e14;
         top: 0;
-      }
-      .subscribe-form button i {
-      color: #fff;
-      font-size: 22px;
-      transform: rotate(-6deg);
-      }
-      .copyright-area{
-      background: #202020;
-      padding: 25px 0;
-      }
-      .copyright-text p {
-      margin: 0;
-      font-size: 14px;
-      color: #878787;
-      }
-      .copyright-text p a{
-      color: #ff5e14;
-      }
-      .footer-menu li {
-      display: inline-block;
-      margin-left: 20px;
-      }
-      .footer-menu li:hover a{
-      color: #ff5e14;
-      }
-      .footer-menu li a {
-      font-size: 14px;
-      color: #878787;
-      }
-      .fa-github{
+    }
+
+    .subscribe-form button i {
+        color: #fff;
+        font-size: 22px;
+        transform: rotate(-6deg);
+    }
+
+    .copyright-area {
+        background: #202020;
+        padding: 25px 0;
+    }
+
+    .copyright-text p {
+        margin: 0;
+        font-size: 14px;
+        color: #878787;
+    }
+
+    .copyright-text p a {
+        color: #ff5e14;
+    }
+
+    .footer-menu li {
+        display: inline-block;
+        margin-left: 20px;
+    }
+
+    .footer-menu li:hover a {
+        color: #ff5e14;
+    }
+
+    .footer-menu li a {
+        font-size: 14px;
+        color: #878787;
+    }
+
+    .fa-github {
         border: solid #fff;
-      }
-  </style>
-  <body>
+    }
+
+    
+</style>
+
+</head>
+
+
+<body>
     <footer class="footer-section">
         <div class="container">
             <div class="footer-cta pt-5 pb-5">
@@ -184,13 +218,13 @@
                             <i class="fas fa-map-marker-alt"></i>
                             <div class="cta-text">
                                 <h4>Find us</h4>
-                                <span>11,Near VIT college,Pune,India-411037</span>
+                                <span>Block-11,Near VIT college,Pune,India-411037</span>
                             </div>
                         </div>
                     </div>
                     <div class="col-xl-4 col-md-4 mb-30">
                         <div class="single-cta">
-                            <i class="fas fa-phone"></i>
+                            <i class="fas fa-phone-alt"></i>
                             <div class="cta-text">
                                 <h4>Call us</h4>
                                 <span>+91-7972043343</span>
@@ -220,11 +254,11 @@
                             </div>
                             <div class="footer-social-icon">
                                 <span>Follow us</span>
-                                <a href="https://www.facebook.com/profile.php?id=100004673267320"><i class="fab fa-facebook-f facebook-bg"></i></a>
-                                <a href="https://twitter.com/vinayaklkokane?s=08"><i class="fab fa-twitter twitter-bg"></i></a>
-                                <a href="mailto:vinayaklkokane2001@gmail.com"><i class="fab fa-google-plus-g google-bg"></i></a>
-                                <a href="https://github.com/koredhanraj431/Car-Rental-System"><i class="fab fa-github"></i></a>
-                                <a href="https://www.instagram.com/vin_vk3103/"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+                                <a href="https://www.facebook.com/profile.php?id=100004673267320"><i class="fab fa-facebook-f fa-2x facebook-bg "></i></a>
+                                <a href="https://twitter.com/vinayaklkokane?s=08"><i class="fab fa-twitter twitter-bg fa-2x"></i></a>
+                                <a href="mailto:vinayaklkokane2001@gmail.com"><i class="fab fa-google-plus-g google-bg fa-2x"></i></a>
+                                <a href="https://github.com/koredhanraj431/Car-Rental-System"><i class="fab fa-github fa-2x"></i></a>
+                                <a href="https://www.instagram.com/vin_vk3103/"><i class="fab fa-instagram instagram-bg fa-2x"></i></a>
                             </div>
                         </div>
                     </div>
@@ -266,7 +300,7 @@
                 <div class="row">
                     <div class="col-xl-6 col-lg-6 text-center text-lg-left">
                         <div class="copyright-text">
-                            <p>Copyright &copy; 2020 Drive Your Dreams India Pvt.Ltd. All Right Reserved.</p>
+                            <p>Copyright &copy; 2020 Drive Your Dreams India Pvt.Ltd. All Rights Reserved!</p>
                         </div>
                     </div>
                     <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
@@ -284,5 +318,6 @@
             </div>
         </div>
     </footer>
-  </body>
+</body>
+
 </html>
