2f20e36907bdf66ad590d1eee03d1dff87521c61
vinayakkokane <51049381+vinayakkokane@users.noreply.github.com>
2020-12-04 23:19:21 +0530

    Update PostTestimonial.php

--- Car-Rental-System(EDI)/User/PostTestimonial.php
+++ Car-Rental-System(EDI)/User/PostTestimonial.php
@@ -47,11 +47,11 @@
             <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
               <option selected>Choose...</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-              <option value="4">Four</option>
-              <option value="5">Five</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
             </select>
           </div>
         </div>
