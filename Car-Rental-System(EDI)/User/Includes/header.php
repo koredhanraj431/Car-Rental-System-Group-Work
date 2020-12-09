202a7f3e9ae422781d5610ebf4c23b02cebb40c9
koredhanraj431 <54303198+koredhanraj431@users.noreply.github.com>
2020-12-09 21:00:23 +0530

    Add files via upload

--- Car-Rental-System(EDI)/User/Includes/header.php
+++ Car-Rental-System(EDI)/User/Includes/header.php
@@ -6,7 +6,7 @@
 <div class="container">
         
 <a class="navbar-brand" href="index.php">
-<img style="height: 50px; width: 70px; " src="images/logo1.png" alt=""> Drive Your Dreams</a>
+<img style="height: 50px; width: 70px; " src="/projects/Car-Rental-System(EDI)/User/images/logo1.png" alt=""> Drive Your Dreams</a>
         
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -14,16 +14,16 @@
         <div class="collapse navbar-collapse navitems" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="user.php">Home<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="/projects/Car-Rental-System(EDI)/User/user.php">Home<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="aboutus.php">About us</a>
+                    <a class="nav-link" href="/projects/Car-Rental-System(EDI)/User/aboutus.php">About us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.php">Contact us</a>
+                    <a class="nav-link" href="/projects/Car-Rental-System(EDI)/User/contact.php">Contact us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="carvarieties.php">Explore cars</a>
+                    <a class="nav-link" href="/projects/Car-Rental-System(EDI)/User/carvarieties.php">Explore cars</a>
                 </li>
 
                 <li class="nav-item dropdown">
@@ -32,12 +32,12 @@
                         <?php  echo $_SESSION["username"]; ?>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="profile.php">Profile Settings</a>
-                        <a class="dropdown-item" href="updpwd.php">Update Password </a>
-                        <a class="dropdown-item" href="#">My Bookings</a>
-                        <a class="dropdown-item" href="PostTestimonial.php">Post A Testimonal</a>
+                        <a class="dropdown-item" href="/projects/Car-Rental-System(EDI)/User/profile.php">Profile Settings</a>
+                        <a class="dropdown-item" href="/projects/Car-Rental-System(EDI)/User/updpwd.php">Update Password </a>
+                        <a class="dropdown-item" href="/projects/Car-Rental-System(EDI)/User/mybookings.php">My Bookings</a>
+                        <a class="dropdown-item" href="#">Post A Testimonal</a>
                         <a class="dropdown-item" href="#">My Testimonal</a>
-                        <a class="dropdown-item" href="logout.php?logout-submit=logout">Logout</a>
+                        <a class="dropdown-item" href="/projects/Car-Rental-System(EDI)/User/logout.php?logout-submit=logout">Logout</a>
                     </div>
                 </li>
             </ul>
