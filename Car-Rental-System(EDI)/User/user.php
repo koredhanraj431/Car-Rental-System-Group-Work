dfda023e68ee92e063eadf6ec8cf723bb5a57810
vinayakkokane <51049381+vinayakkokane@users.noreply.github.com>
2020-12-10 16:59:31 +0530

    Update user.php

--- Car-Rental-System(EDI)/User/user.php
+++ Car-Rental-System(EDI)/User/user.php
@@ -1,83 +1,55 @@
 <?php
-session_start();
-if (!isset($_SESSION['username'])) {
-  $_SESSION['msg'] = "You must log in first";
-  header('location: index.php');
+include('Includes/server.php');
+
+if (isset($_SESSION['username'])) {
+  header('location: user.php');
 }
 
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-  <!-- FontAwesome -->
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-  <script src="https://kit.fontawesome.com/3695790714.js" crossorigin="anonymous"></script>
-  <link rel="icon" href="images/favicon-16x16.png">
-  <link rel="stylesheet" href="CSS/styles.css">
-  <title>Welcome, <?php  echo $_SESSION["username"]; ?></title>
-
-</head>
-
-<body>
-
-  <header>
-    <?php
-    include('Includes/header.php');
-    ?>
-  </header>
-
-  <!-- Sliding images -->
-  <?php
-  include('Includes/carousel.php');
-  ?>
-
-  <!-- container marketing -->
-  <div class="container marketing d-flex justify-content-center">
-    <div class="row py-4">
-      <div class="col-lg-4">
-        <img class="img-circle" src="images/explore.png" alt="Explore">
-        <h2>Explore</h2>
-        <p></p>
-        <p>
-          <a class="btn btn-default btn-outline-success" href="carvarieties.php" role="button">View details>></a>
-        </p>
-      </div>
-      <div class="col-lg-4">
-        <img class="img-circle" src="images/contact.png" alt="Contact">
-        <h2>Contact</h2>
-        <p></p>
-        <p>
-          <a class="btn btn-default btn-outline-success" href="contact.php" role="button">View details>></a>
-        </p>
-      </div>
-      <div class="col-lg-4">
-        <img class="img-circle" src="images/about.jpg" alt="about us">
-        <h2>AboutUs</h2>
-        <p></p>
-        <p>
-          <a class="btn btn-default btn-outline-success" href="aboutus.php" role="button">View details>></a>
-        </p>
-      </div>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <script src="https://kit.fontawesome.com/3695790714.js" crossorigin="anonymous"></script>
+    <link rel="icon" href="images/favicon-16x16.png">
+    <link rel="stylesheet" href="CSS/login_reg.css">
+    <title>Login-Drive Your Dreams</title>
+    <style media="screen">
+      .btn{
+        border:  2px #000;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="text-lg mb-3 text-center mt-5 text-success" data-test="welcome-message"><strong>Get Started with the leading Car rental Platform in India, Drive Your Dreams!</strong></div>
+    <div class="header">
+    	<h2>Login</h2>
     </div>
-  </div>
-  <footer>
-    <?php
-      include('includes/footer.php');
-    ?>
-  </footer>
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-  
-</body>
 
+    <form method="post" action="login.php">
+    	<?php include('Includes/errors.php'); ?>
+    	<div class="input-group">
+    		<i class="fas fa-user py-2"></i><label>Username</label>
+    		<input type="text" name="username" >
+    	</div>
+    	<div class="input-group">
+    		<i class="fas fa-lock prefix py-2"></i><label>Password</label>
+    		<input type="password" name="password">
+    	</div>
+      <div class="input-group">
+        <a href="recover_email.php">Forgot Password?</a>
+      </div>
+    	<div class="input-group logIn">
+    		<button type="submit" class="btn" name="login_user">Login</button>
+    	</div>
+    	<p>
+    		Not yet a member? <a href="register.php">Sign up</a>
+    	</p>
+    </form>
+  </body>
 </html>
